3b12d97639f24645cf8eadb586886608620404be
Christina <chu.christina@yahoo.com>
2019-03-10 22:39:19 -0700

    Add merge two sorted lists exercise

---
+++ merge_two_sorted_lists.js
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+// /**
+//  * @param {ListNode} l1
+//  * @param {ListNode} l2
+//  * @return {ListNode}
+//  */
+
+
+const mergeTwoLists = function (l1, l2) {
+  const headNode = new ListNode(); 
+  let currNode = headNode; 
+
+  while (l1 || l2) {
+    let nextNode; 
+
+    if (l1 && l2) {
+      nextNode = Math.min(l1.val, l2.val) === l1.val ? l1 : l2;
+      if (nextNode === l1) l1 = l1.next; 
+      if (nextNode === l2) l2 = l2.next; 
+    } else {
+      nextNode = l1 ? l1 : l2; 
+      if (nextNode === l1) l1 = l1.next;
+      if (nextNode === l2) l2 = l2.next; 
+    }
+
+    currNode.next = nextNode; 
+    currNode = currNode.next; 
+  }
+
+  return headNode.next; 
+};
