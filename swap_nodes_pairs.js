b4f765754d82699bdb37bf2f24ba80011d9beefb
Christina <chu.christina@yahoo.com>
2019-03-11 22:51:30 -0700

    Add swap node pairs exercise

---
+++ swap_nodes_pairs.js
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/swap-nodes-in-pairs/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+// /**
+//  * @param {ListNode} head
+//  * @return {ListNode}
+//  */
+
+const swapPairs = function (head) {
+  if (!head) return head;
+
+  const headNode = new ListNode();
+  headNode.next = head; 
+
+  let prevIterationNode = headNode;  
+  let currNode = head; 
+
+  while (currNode.next) {
+    let firstNode = currNode; 
+
+    currNode = currNode.next; 
+    firstNode.next = currNode.next; 
+    currNode.next = firstNode;
+    
+    prevIterationNode.next = currNode;
+    prevIterationNode = firstNode; 
+
+    if (firstNode.next) {
+      currNode = firstNode.next;
+    } else {
+      break; 
+    }
+  }
+
+  return headNode.next; 
+};
